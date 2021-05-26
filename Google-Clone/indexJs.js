b9ae4112c372fd7e03bc7fcb5a955e07da96a0b7
SantoshJonnakuti <santoshjonnakuti@gmail.com>
2021-05-26 10:52:47 +0530

    Added-Feeling-Lucky-Functionality

--- Google-Clone/indexJs.js
+++ Google-Clone/indexJs.js
@@ -3,3 +3,7 @@ function googleSearch()
   const search = document.getElementsByName("SearchBar")[0].value;
   window.location.href = "https://www.google.com/search?q="+ search;
 }
+function googleFeelingLucky()
+{
+  window.location.href = "https://www.google.com/doodles";
+}

803baa3f39749a6a4be7d426b69c2a6ab024d2df
SantoshJonnakuti <santoshjonnakuti@gmail.com>
2021-05-26 14:53:07 +0530

    Removed-Else

--- Google-Clone/indexJs.js
+++ Google-Clone/indexJs.js
@@ -3,9 +3,9 @@ function googleSearch() {
   if (const) {
     window.location.href = "https://www.google.com/search?q="+ search;
   }
-  else {
-    window.location.href = "https://www.google.com/";
-  }
+  // else {
+  //   window.location.href = "https://www.google.com/";
+  // }
 }
 function googleFeelingLucky() {
   window.location.href = "https://www.google.com/doodles";
